b6e1ba762d85f876dfbb9494f1b49959f290f900
KevinZSJ <44286780+KevinZSJ@users.noreply.github.com>
2018-10-19 19:40:18 +0800

    Update A.java

--- src/main/java/com/baizhi/test/A.java
+++ src/main/java/com/baizhi/test/A.java
@@ -6,7 +6,7 @@ package com.baizhi.test;
 public class A {
     public static void main(String[] args) {
         System.out.println("呵呵");
-        System.out.println("刘时勋 智障");
+        System.out.println("呵呵");
 
     }
 }
