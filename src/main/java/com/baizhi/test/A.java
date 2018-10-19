817b2744ea5ad7b5666a537e5194da546486a597
ZSJ <2537080436@qq.com>
2018-10-19 17:53:05 +0800

    第二次提交

---
+++ src/main/java/com/baizhi/test/A.java
@@ -0,0 +1,12 @@
+package com.baizhi.test;
+
+/**
+ * Created by Administrator on 2018/10/19.
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("呵呵");
+        System.out.println("刘时勋智障");
+
+    }
+}
